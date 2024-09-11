518e5618726006e8a994e0aacb9b627e823c7afe
FabianeML <fabiane_stargirl@hotmail.com>
2024-09-11 10:01:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,16 +26,7 @@ def main():
         #Faça modificações apenas na seção do seu número
         #Faça o print de 10 filmes do gênero selecionado
         if escolha == 1:
-            print("Batman: O Cavaleiro das Trevas (2008)")
-            print("A Origem (2010)")
-            print("Matrix (1999)")
-            print("O Senhor dos Anéis: O Retorno do Rei (2003)")
-            print("Gladiador (2000)")
-            print("Duro de Matar (1988)")
-            print("Mad Max: Estrada da Fúria (2015)")
-            print("Os Vingadores (2012)")
-            print("John Wick: De Volta ao Jogo (2014)")
-            print("O Exterminador do Futuro 2: O Julgamento Final (1991)")
+            pass
         elif escolha == 2:
             pass
         elif escolha == 3:
@@ -59,7 +50,22 @@ def main():
         elif escolha == 12:
             pass
         elif escolha == 13:
-            pass
+             # Lista de 10 filmes musicais
+            musical_films = [
+                "The Sound of Music",
+                "Singin' in the Rain",
+                "Grease",
+                "West Side Story",
+                "Chicago",
+                "La La Land",
+                "Mamma Mia!",
+                "Les Misérables",
+                "The Rocky Horror Picture Show",
+                "Hairspray"
+            ]
+
+            for film in musical_films:
+                print(f"- {film}")
         elif escolha == 14:
             pass
         elif escolha == 15:
